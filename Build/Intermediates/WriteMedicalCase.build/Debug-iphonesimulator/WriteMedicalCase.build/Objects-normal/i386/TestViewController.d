d778c52577e86b54074150c7e6088d0b4bc3abfe
user1 <register6909@gmail.com>
2015-06-03 16:00:42 +0800

    增加创建笔记是的删除

---
+++ Build/Intermediates/WriteMedicalCase.build/Debug-iphonesimulator/WriteMedicalCase.build/Objects-normal/i386/TestViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/gk/Documents/病历/medical/WriteMedicalCase/TestViewController.m \
+  /Users/gk/Documents/病历/medical/WriteMedicalCase/TestViewController.h
---
+++ WriteMedicalCase/TestViewController.m
@@ -0,0 +1,42 @@
+//
+//  TestViewController.m
+//  WriteMedicalCase
+//
+//  Created by ihefe-JF on 15/6/3.
+//  Copyright (c) 2015年 GK. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+///for unwind segue 
+-(IBAction)createNoteCancel:(UIStoryboardSegue*)segue
+{
+    
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
